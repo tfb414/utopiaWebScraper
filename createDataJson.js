ba162a9478074b8bd6be249853288c78dac71bbc
Tim Brady <tfb414@gmail.com>
2018-05-27 18:24:15 -0400

    read file and write additional data

--- createDataJson.js
+++ createDataJson.js
@@ -1,22 +1,84 @@
-// import {main} from 'grabTotals';
+fs = require('fs');
 const grabTotals = require("./grabTotals.js");
 
 async function main() {
-  await retreiveData();
+  const data = await retreiveData();
+  const formattedData = formatData(data, removeCommas);
+
+  // const fakeData = { "landAverage": "3333", "landTotal": "40999", "networthAverage": "293629gc", "networthTotal": "6753469gc", "honorTotal": "72621" }
+  readMasterDataAndAddNewData(formattedData);
+  // writeFile(formattedData)
 }
 
 async function retreiveData() {
-  // const data = await grabTotals.main();
-  // console.log(data);
-
-  const data = { landTotal: '40,193 acres (avg: 1,747 acres)',
-  networthTotal: '6,753,469gc (avg: 293,629gc)',
-  honorTotal: '72,621' }
-
-  console.log(data);
+  // return await grabTotals.main();
+  return {
+    landTotal: '22,222 acres (avg: 1,748 acres)',
+    networthTotal: '6,753,469gc (avg: 293,629gc)',
+    honorTotal: '72,621'
+  }
 }
 
-function cleanData() {
+function formatData(data, fn) {
+  const formattedData = {};
 
+  const landTotalString = data['landTotal'];
+  const landTotalNumbers = selectNumbers(removeCommas(landTotalString));
+  const landAverage = landTotalNumbers[1];
+  const landTotal = landTotalNumbers[0];
+
+  const networthTotalString = data['networthTotal'];
+  const networthTotalNumbers = selectNumbers(removeCommas(networthTotalString));
+  const networthAverage = networthTotalNumbers[1];
+  const networthTotal = networthTotalNumbers[0];
+
+  formattedData['landAverage'] = landAverage;
+  formattedData['landTotal'] = landTotal;
+  formattedData['networthAverage'] = networthAverage;
+  formattedData['networthTotal'] = networthTotal;
+  formattedData['honorTotal'] = removeCommas(data['honorTotal']);
+
+  return formattedData;
 }
+
+function removeCommas(string) {
+  return string.replace(/,\s?/g, "");
+}
+
+function selectNumbers(string) {
+  const regExp = /([1-9])\w+/g;
+  return string.match(regExp);
+}
+
+
+//move this to another file
+//you will need to validate the file is larger after you've written in it
+async function readMasterDataAndAddNewData(newData) {
+  fs.readFile('data.txt', 'utf8', function (err, masterData) {
+    if (err) {
+      return console.log(err);
+    }
+    let updatedMasterData = addNewDataToSavedData(newData, masterData);
+    writeFile(updatedMasterData);
+  });
+}
+
+function writeFile(data) {
+  fs.writeFile('data.txt', JSON.stringify(data), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  })
+}
+
+function addNewDataToSavedData(newData, masterData) {
+  let updatedMasterData = JSON.parse(masterData);
+
+  for (key in updatedMasterData) {
+    const oldValue = updatedMasterData[key]
+    oldValue.push(newData[key]);
+    updatedMasterData[key] = oldValue;
+  }
+  return updatedMasterData;
+}
+
 main();
